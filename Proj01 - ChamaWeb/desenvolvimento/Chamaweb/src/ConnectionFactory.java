cbaef8a6760d6e3817ef9522465d876b305b4a1a
Gustavo Mansur <gurmansur@hotmail.com>
2023-10-30 20:47:37 -0300

    Adicionados Templates do Backend Chamaweb

---
+++ Proj01 - ChamaWeb/desenvolvimento/Chamaweb/src/ConnectionFactory.java
@@ -0,0 +1,30 @@
+package vendaprodutos.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Uma fábrica de conexões.
+ *
+ * @author Gustavo Mansur
+ */
+public class ConnectionFactory {
+
+    /**
+     * O método getConnection retorna uma conexão com a base de dados
+     * venda_produtos.
+     *
+     * @return Uma conexão com o banco de dados chamaweb.
+     * @throws SQLException Caso ocorra algum problema durante a conexão.
+     */
+    public static Connection getConnection() throws SQLException {
+
+        return DriverManager.getConnection(
+                "jdbc:mariadb://localhost/chamaweb",
+                "root",
+                "" );
+
+    }
+
+}
