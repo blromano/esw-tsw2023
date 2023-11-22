ea8a64a0555085a515d1b67d3e9f22d5e6c96fa9
Felipe <61094586+nascimento-felipe@users.noreply.github.com>
2023-11-22 01:08:29 -0300

    criar mensagem

--- Proj04 - Devden/desenvolvimento/devden_backend/src/app.module.ts
+++ Proj04 - Devden/desenvolvimento/devden_backend/src/app.module.ts
@@ -10,10 +10,12 @@ import { LogarController } from './controllers/logar/logar.controller';
 import { LogarService } from './services/logar/logar.service';
 import { CriarTopicoService } from './services/criar-topico/criar-topico.service';
 import { CriarTopicoController } from './controllers/criar-topico/criar-topico.controller';
+import { EnviarMensagemService } from './services/enviar-mensagem/enviar-mensagem.service';
+import { EnviarMensagemController } from './controllers/enviar-mensagem/enviar-mensagem.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController, CriarUsuarioController, LogarController, CriarTopicoController],
-  providers: [AppService, UserService, PrismaService, CriarUsuarioService, LogarService, CriarTopicoService],
+  controllers: [AppController, UserController, CriarUsuarioController, LogarController, CriarTopicoController, EnviarMensagemController],
+  providers: [AppService, UserService, PrismaService, CriarUsuarioService, LogarService, CriarTopicoService, EnviarMensagemService],
 })
 export class AppModule {}
